2c6a45079724556b6c4b2dd77973443861386c29
Сабина Ахметова <sabina.akhmetova@kmf.kz>
2024-01-22 18:03:15 +0600

    #auth_page

--- functional/android/pages/auth_page.py
+++ functional/android/pages/auth_page.py
@@ -1,6 +1,10 @@
 from functional.android.PageFactory_android.find_accesibility_id import *
 
 class AuthPage(Component):
-    def __init__(self, number=None, password=None):
+    def __init__(self, value=None, number=None, password=None):
         self.number = number
         self.password = password
+        super().__init__(value)
+
+    def sign_up(self,driver):
+        pass
